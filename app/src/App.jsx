6d61664f477ead26bdb99eb43bf5085d1b7d3c63
Jeremiah de Guzman <124826126+jerrymiahdg@users.noreply.github.com>
2024-03-21 00:01:24 +0000

    commit

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from "/vite.svg";
 import data from "./markdown/md.md";
 import ReactMarkdown from "react-markdown";
 import "./App.css";
+import Nav from "./components/Nav";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -14,8 +15,9 @@ function App() {
 
   return (
     <>
-      <div className="flex justify-center py-56">
-        <ReactMarkdown className="max-w-4xl w-full text-left prose prose-invert">
+      <Nav />
+      <div className="flex justify-center py-56 px-5">
+        <ReactMarkdown className="max-w-4xl w-full text-left prose prose-h1:text-green-950 prose-h2:text-green-950 prose-p:text-green-950">
           {data}
         </ReactMarkdown>
       </div>
